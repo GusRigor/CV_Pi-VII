48f7360f80b2af4fcd7630db41632ea87a3486b4
Gustavo Rigor <gusrigor@icloud.com>
2021-05-06 20:24:03 -0300

    Att: url para envio

--- Smart Belt/request.py
+++ Smart Belt/request.py
@@ -2,16 +2,15 @@ import requests
 
 class request:
     def __init__(self, productCondition = "OK"):
-        self.url = "https://smart-b-api.ue.r.appspot.com/"
+        self.url = "https://smart-b-api.ue.r.appspot.com/api/v1/product/register"
         self.productType = "BANANA"
         self.productCondition = productCondition
 
     def post(self, quantity):
-        data = {
-            "userId": "17948f0c-9eb3-40b5-923e-28a5a346dadd",
+        r = requests.post(self.url, json = {
+            "user": "17948f0c-9eb3-40b5-923e-28a5a346dadd",
             "productType": self.productType,
             "quantity": quantity,
             "productCondition": self.productCondition
         }
-        requests.post(self.url, data=data)
-
+        )
